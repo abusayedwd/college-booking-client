8c5ac0a29365e5f1fe1525233c3922f9248d22cf
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-07-22 22:40:04 +0600

    added project

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import Header from '../Components/Home/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Home/Footer';
+
+const Main = () => {
+        return (
+                <div>
+                     <Header></Header> 
+                     <Outlet></Outlet>
+                     <Footer></Footer>  
+                </div>
+        );
+};
+
+export default Main;
